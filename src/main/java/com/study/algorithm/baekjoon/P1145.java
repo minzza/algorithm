0ce1a7cc57c153484383f025fba5b52c71845c5a
Minzza <cmjasdf@gmail.com>
2022-02-06 21:44:49 +0900

    [1145] 적어도 대부분의 배수

---
+++ src/main/java/com/study/algorithm/baekjoon/P1145.java
@@ -0,0 +1,34 @@
+package com.study.algorithm.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class P1145 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int value = 1;
+        int cnt = 0;
+        while (true) {
+            for (int i = 0; i < arr.length; i++) {
+                if (value % arr[i] == 0) {
+                    cnt++;
+                }
+            }
+            if (cnt >= 3) {
+                System.out.println(value);
+                break;
+            }
+            cnt = 0;
+            value++;
+        }
+    }
+}
